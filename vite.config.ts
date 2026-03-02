ce2b7a62a07e1204f90bbc60d00c2277c9451f69
Igor Somov <donprusne@gmail.com>
2026-03-01 23:00:24 -0300

    chore: centralize tab metadata and gate testing plugin to test mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(async ({ mode }) => {
+	const plugins: PluginOption[] = [sveltekit()];
+
+	if (mode === 'test') {
+		const { svelteTesting } = await import('@testing-library/svelte/vite');
+		plugins.push(svelteTesting());
+	}
+
+	return { plugins };
 });
